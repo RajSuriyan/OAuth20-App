f27d54f1fd8e54a5e7e214f0a3bb5b80e606c982
RajSuriyan <rajsuriyan200110@gmail.com>
2022-07-15 18:14:28 +0530

    Bcrypt has been added and GitIgnored

---
+++ app.js
@@ -0,0 +1,101 @@
+//jshint esversion:6
+require("dotenv").config();
+const bcrypt=require("bcrypt");
+const express=require('express');
+const bodyParser=require('body-parser');
+const ejs=require("ejs");
+const mongoose=require("mongoose");
+const encrypt=require("mongoose-encryption");
+const saltrounds=10;
+const app=express();
+
+app.use(express.static(__dirname+"/public"));
+app.set("view engine","ejs");
+app.use(bodyParser.urlencoded({extended:true}));
+
+mongoose.connect("mongodb://localhost:27017/usersDB");
+
+const usersSchema=new mongoose.Schema({
+    email:String,
+    password:String
+});
+
+usersSchema.plugin(encrypt,{secret:process.env.SECRET,excludeFromEncryption: ['email']})
+
+
+const User=mongoose.model("User",usersSchema);
+
+
+
+app.get("/",function(req,res){
+    res.render("home");
+});
+app.get("/login",function(req,res){
+    res.render("login");
+});
+app.get("/register",function(req,res){
+    res.render("register");
+});
+
+
+
+app.post("/register",function(req,res){
+    bcrypt.hash(req.body.password,saltrounds,function(err,hash){
+    const newUser=new User({
+        email:req.body.username,
+        password:hash
+    });
+    
+    
+    newUser.save(function(err){
+        if(err){
+            console.log(err);
+        }else{
+            res.render("secrets");
+        }
+    });
+});
+
+});
+
+app.post("/login",function(req,res){
+    
+  
+        
+        const userName=req.body.username;
+        const password = req.body.password;
+        User.findOne({email:userName},function(err,foundUser){
+            if(err){
+                console.log(err);
+            }else{
+                if(foundUser){
+                    
+                    bcrypt.compare(password, foundUser.password,function(err,result){
+                        if(result===true){
+                            res.render("secrets");
+                        }else{
+                            res.send("Incorrect Password!");
+                        }
+                    });
+                }else{
+                    res.send("Invalid User Name");
+                }
+            }
+        });
+    
+    
+});
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000,function(){
+    console.log("Server listening on port 3000");
+});
